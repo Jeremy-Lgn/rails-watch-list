ddd336613c2824d13787abb4212581d64bb0d235
Jeremy Ligaon <jeremy.ligaon@gmail.com>
2025-02-28 17:03:06 +0100

    OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "lists#index"
 
   resources :lists, only: %i[index new show create] do
     resources :bookmarks, only: %i[create]
