bc1170e90e87978e7ba05fa2258ce11dbaa8b554
Jeremy Ligaon <jeremy.ligaon@gmail.com>
2025-02-28 11:39:16 +0100

    psql

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+
+    if @bookmark.save
+      redirect_to list_path(@list), notice: "Bookmark was successfully created."
+    else
+      render "lists/show", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+end
